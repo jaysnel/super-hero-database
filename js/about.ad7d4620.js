e63797d0bf5029dc393d8165b22154e7915ccf83
jaysnel <jaylan210@gmail.com>
2020-06-15 15:13:24 -0400

    initial dist subtree commit

---
+++ js/about.ad7d4620.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"0f38":function(t,e,n){},"2d42":function(t,e,n){"use strict";var c=n("0f38"),a=n.n(c);a.a},f820:function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About")])])}],u=(n("2d42"),n("2877")),s={},r=Object(u["a"])(s,c,a,!1,null,"4bc8eace",null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.ad7d4620.js.map
